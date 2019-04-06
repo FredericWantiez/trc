acaf77dfbed0a681e86102ce4f836b4935f3f3e5
frederic <frederic.wantiez@gmail.com>
2019-04-06 09:54:54 +0100

    Updates

---
+++ precache-manifest.479105e68b7c827600b8e14b456fe524.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b92663d35dac23f7af8",
+    "url": "/FredericWantiez/trc/static/js/runtime~main.b370d3fa.js"
+  },
+  {
+    "revision": "4512de254d770f36b53c",
+    "url": "/FredericWantiez/trc/static/js/main.4a4fe38a.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/FredericWantiez/trc/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "4512de254d770f36b53c",
+    "url": "/FredericWantiez/trc/static/css/main.b9e19a4c.chunk.css"
+  },
+  {
+    "revision": "0ad63ee666f465a992e4e858c7a2d052",
+    "url": "/FredericWantiez/trc/index.html"
+  }
+];
